ae72f27d291cae156ab79e0e12a57963cfc353b1
TTsham6 <37510144+TTsham6@users.noreply.github.com>
2020-12-04 15:32:48 +0900

    first commit

---
+++ kouryaku-spring/src/main/java/com/jp/kouryakuspring/KouryakuSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jp.kouryakuspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KouryakuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KouryakuSpringApplication.class, args);
+	}
+
+}
---
+++ kouryaku-spring/src/test/java/com/jp/kouryakuspring/KouryakuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.kouryakuspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KouryakuSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
